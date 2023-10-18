2160ffcd1840df9b9f4577d98657a6382bde6755
Nino Kilasonia <kilasonia.nino@kiu.edu.ge>
2023-10-18 19:10:33 +0400

    //

--- src/w3array/GamesWithArrays.java
+++ src/w3array/GamesWithArrays.java
@@ -130,7 +130,7 @@ public class GamesWithArrays {
 
 
     for (int i = 0; i < len - 1; i++) {
-      if (resArr[i+1] % resArr[i] == 0 && resArr[i] != resArr[i+1]) return resArr[i];
+      if ((resArr[i] % resArr[i+1] == 0 || resArr[i+1] % resArr[i] == 0) && resArr[i] != resArr[i+1]) return Math.min(resArr[i], resArr[i+1]);
       if (resArr[i] != resArr[i + 1]) return 0;
     }
 
